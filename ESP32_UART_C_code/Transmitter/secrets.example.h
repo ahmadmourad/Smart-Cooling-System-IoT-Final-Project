9d36985dc2bbcea4359c2205311e6fcf7d87c944
ahmadmourad <workwithahmedm@gmail.com>
2024-08-27 04:41:19 +0300

    added example file for secrets file
    note to use the file copy secrets.example.h file to secrets.h then put your
    credentials

---
+++ ESP32_UART_C_code/Transmitter/secrets.example.h
@@ -0,0 +1,10 @@
+// Use this file to store all of the private credentials
+// and connection details
+
+#define YOUR_SSID "MySSID"     // replace MySSID with your WiFi network name
+#define YOUR_PASS "MyPassword" // replace MyPassword with your WiFi password
+
+#define YOUR_GOOGLE_APIKEY "MYAPIKEY" // replace 'MYAPIKEY' with your google api key
+#define DB_URL "XYZ"                  // replace XYZ with your realtime firebase database
+#define FIREBASE_EMAIL "XYZ"              // replace XYZ with your user email of firebase
+#define FIREBASE_PASS "XYZ"               // replace XYZ with your password of firebase account
